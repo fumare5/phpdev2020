b9c3c86d9930a25a5e4453bae5ab2dbe4d9d1178
fumare5 <danijelfumic@gmail.com>
2019-10-19 10:48:32 +0200

    prva php stranica

--- hello.php
+++ hello.php
@@ -1,8 +1,9 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "Helloooo!!!!";
+for ($index = 0; $index < 100; $index++) {
+    echo "Heloooo!!!!!";
+} 
