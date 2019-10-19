402879aa59ef2770100ae7a160ae9a2177502f73
fumare5 <danijelfumic@gmail.com>
2019-10-19 15:27:22 +0200

    liniski komentari

---
+++ 1.5-temeljna-pravila.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* blok komentar */
+/* još jedan blok
+ * još
+ * još
+ * još
+ * još je zadni blok
+ * ....
+ */
+// linijski komentar
+# još jedan linijski komentar
+?>
+
+<p> Ovo je neki html paragraf....</p><br>
+<p style="font-weight: bolder "> Ovo je neki drugi html paragraf</p><br>
+
+<?php
+echo "Ovo je php!!!!";
+ECHO "Ovo je php!!!!";
+eCHo "Ovo je php!!!!";
+?>
+<?="Ovo je skraceno" ?>
+
+<p style="font-weight: bold"> Natrag na html</p><br>
