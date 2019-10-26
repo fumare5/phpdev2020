95b3a2f79150405e364fd8fe1affb59e0a325394
fumare5 <danijelfumic@gmail.com>
2019-10-26 16:35:31 +0200

    Zadaci za ponavljanje

--- 5.7_zadaci_za_ponavljanje.php
+++ 5.7_zadaci_za_ponavljanje.php
@@ -6,6 +6,7 @@
  */
 echo 'Riješenje za zadatak 5.7.1';
 echo "<br>";
+echo "<br>";
 
 $a = 4;
 $b = 2;
@@ -15,3 +16,68 @@ if($a < $b)
 
 if($a > $b)
     echo $a." je veći od ".$b;
+echo "<br>";
+echo "<hr>";
+
+/* 
+ * 2. Napišite program koji rabi dvije varijable $a i $b. Definirajte im proizvodnje brojčane vrijednosti.
+ * Ako je $a veće od $b, onda uvećajte vrijednost $a za jedan, a ako je veća varijabla $b, onda je umanjite za jedan
+ * koristeći se operatorima automatskog povećavanja ili smanjivanja.
+ * Ispišite na kraju vrijednosti varijabli.
+ */
+
+unset($a,$b);
+
+$a = 8;
+$b = 10;
+
+echo 'Riješenje za zadatak 5.7.2';
+echo "<br>";
+echo "<br>";
+
+if ($a > $b) $a++;
+else { $a--;
+}
+echo $a."  ".$b;
+
+echo "<br>";
+echo "<hr>";
+
+/* 
+ * 3. Pomoću petlje while ispišite od 1 do 50 na ekranu, jedan ispod drugog.
+ */
+
+unset($a,);
+
+echo 'Riješenje za zadatak 5.7.3';
+echo "<br>";
+echo "<br>";
+
+$a = 1;
+while ($a <= 50)
+{
+    echo $a."<br/>";
+    $a ++;
+}
+
+echo "<br>";
+echo "<hr>";
+
+/* 
+ * 4. S pomoću petlje for zbrojite brojeve od 20 do 30 te zbroj ispišite na ekranu.
+ */
+
+echo 'Riješenje za zadatak 5.7.4';
+echo "<br>";
+echo "<br>";
+
+$zbroj=0;
+for($x=20; $x<=30; $x++)
+{
+    $zbroj +=$x;
+}
+echo "$zbroj";
+
+echo "<br>";
+echo "<hr>";
+echo 'The end!';
