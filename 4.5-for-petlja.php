5aacecc96def9d94b61885d31400c4d9e3c95466
fumare5 <danijelfumic@gmail.com>
2019-10-26 14:26:02 +0200

    For petlja

--- 4.5-for-petlja.php
+++ 4.5-for-petlja.php
@@ -1,7 +1,53 @@
 <?php
-
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * petlja ispisuje brojeve 0,1,2... 99
  */
+for ($index = 0; $index < 100; $index++) {
+    echo $index." ";
+}
+echo "<hr>";
+/* 
+ * petlja ispisuje brojeve 0,1,2... 100
+ */
+for ($index = 0; $index <= 100; $index++) {
+    echo $index." ";
+}
+echo "<hr>";
+/* 
+ * petlja ispisuje brojeve 0,2,4,6 ... 100
+ */
+for ($index = 0; $index <= 100; $index+=2) {
+    echo $index." ";
+}
+echo "<hr>";
+/* 
+ * petlja ispisuje brojeve 0,7,14,21 ... 100
+ */
+for ($index = 0; $index <= 100; $index+=7) {
+    echo $index." ";
+}
+echo "<hr>";
+/* 
+ * petlja ispisuje brojeve -32 -29 -26 -23 -20 ....  19
+ */
+for ($index = -32; $index <= 20; $index+=3) {
+    echo $index." ";
+}
+echo "<hr>";
+/* 
+ * petlja ispisuje brojeve 40, 37, 34, 31 ... 20
+ */
+for ($index = 40; $index >= 20; $index-=3) {
+    echo $index." ";
+}
+echo "<hr>";
+/* 
+ * petlja ispisuje brojeve 0,1,2...  <preskoci sve od 35-77> 99
+ */
+
+for ($index = 0; $index < 100; $index++) {
+    if($index>34 && $index<78){
+        continue;
+    }
+    echo $index." ";
+}
