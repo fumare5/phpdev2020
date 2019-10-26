71a6a35cc5b39ce37747497807a8401aa2bfa9b2
fumare5 <danijelfumic@gmail.com>
2019-10-26 10:53:29 +0200

    Kontrolene strukture

---
+++ 4.2-kontrolne-strukture.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+ * if-else
+ * switch-case
+ * while
+ * for
+ * foreach
+ */
+$a=2;
+if($a==1){
+    echo "Varijabla je jednaka 1";
+}
+ else {
+    echo "Varijabla nije jednaka 1";
+}
+echo "<hr>";
