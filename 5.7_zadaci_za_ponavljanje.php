12535c017a59eac16125c45d7eb9e162620bbbba
fumare5 <danijelfumic@gmail.com>
2019-10-26 14:51:46 +0200

    Zadaci za ponavljanje

---
+++ 5.7_zadaci_za_ponavljanje.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * 1. Koristeći se operatorima usporedbe veće od i manje od i iskazom if-else provjerite
+ * odnos varijabli $a=4 i $b=2 i ispišite prikladnu poruku.
+ */
+echo 'Riješenje za zadatak 5.7.1';
+echo "<br>";
+
+$a = 4;
+$b = 2;
+
+if($a < $b)
+    echo $a." je manji od ".$b;
+
+if($a > $b)
+    echo $a." je veći od ".$b;
