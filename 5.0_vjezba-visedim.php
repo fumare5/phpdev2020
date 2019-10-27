9090291fe76dedb2162353cb6d9a19c6fc2e9b93
fumare5 <danijelfumic@gmail.com>
2019-10-27 14:01:31 +0100

    Primjeri iz knjige višedimenzionalna polja

--- 5.0_vjezba-visedim.php
+++ 5.0_vjezba-visedim.php
@@ -9,3 +9,34 @@ $multi_array = array($elem1, $elem2, $elem3);
 echo '<pre>';
 print_r($multi_array);
 echo '</pre>';
+
+echo '<hr><br>';
+
+echo $multi_array[2][1];//ispisati ce broj 3
+
+echo '<hr><br>';
+
+foreach ($multi_array as $elem)
+{
+    foreach($elem as $val)
+    {
+        echo $val;
+    }
+}
+
+echo '<hr><br>';
+unset($elem1,$elem2,$elem3,$multi_array);
+
+
+$elem1 = array('ime' => 'Petar', 'prezime' => 'Petric', 'MB' => '123');
+$elem2 = array('ime' => 'Ante', 'prezime' => 'Antic', 'MB' => '234');
+
+$multi_array = array('ucenik1' => $elem1, 'ucenik2'=> $elem2);
+
+echo '<pre>';
+print_r($multi_array);
+echo '</pre>';
+
+echo '<hr><br>';
+
+echo $multi_array['ucenik2']['MB'];
