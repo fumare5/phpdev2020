783557dfa75710947f11579cb233b36f17ab1804
fumare5 <danijelfumic@gmail.com>
2019-11-01 16:35:41 +0100

    Zadaci za ponavljanje str 90

---
+++ Zadaci/funkcije_sa_90_str.php
@@ -0,0 +1,28 @@
+<?php
+
+// funkcija is_file
+if(is_file('razred_a.txt'))
+{
+    //blok koda
+}
+
+//funkcija id_dir
+if(is_dir('/var/www/test/'))
+{
+    //blok koda
+}
+
+//funkcija mkdir
+mkdir('/var/www/test/');
+
+//funkcija rmdir
+rmdir('/var/www/test/');
+
+//funkcija unset
+///unset('razred_a.txt.');
+
+$a=2;
+unset($a);
+
+        
+
