9aa815db1653e0c5926760cf421b5ae72335c85c
fumare5 <danijelfumic@gmail.com>
2019-11-12 20:58:00 +0100

    Objekti race

--- objekti_24h_Le_Mans_Endurance_bike_race/example_race.php
+++ objekti_24h_Le_Mans_Endurance_bike_race/example_race.php
@@ -13,19 +13,36 @@ $crew6=new Crew("Yamaha");
 $crew7=new Crew("Kawasaki");
 $crew8=new Crew("Suzuki");
 
+$crew_name1= "WOJCIK RACING TEAM";
+$crew_name2= "TEAM ERC ENDURANCE";
+$crew_name3= "HRT 100 - HERTRAMPF RACING ENDURANCE";
+$crew_name4= "MOTOBOX KREMER RACING";
+$crew_name5= "F.C.C. TSR HONDA FRANCE";
+$crew_name6= "YART YAMAHA";
+$crew_name7= "WEBIKE SRC KAWASAKI FRANCE";
+$crew_name8= "SUZUKI ENDURANCE RACING TEAM";
+        
 
 $race= new Race($crew1, $crew2, $crew3, $crew4, $crew5, $crew6, $crew7, $crew8);
 $race->drive (new DateTime("now"));
 
 
 
+
+echo "<strong>24 Heures Motos 2019 - FIM EWC</strong>";
+echo "<h3>";
 //ispis odveženih krugova u 24h
-echo "Team Aprilia drove ".$race->no_laps1()," laps in 24 hours","<br>";
-echo "Team BMW drove ".$race->no_laps2()," laps in 24 hours","<br>";
-echo "Team Ducati drove ".$race->no_laps3()," laps in 24 hours","<br>";
-echo "Team KTM drove ".$race->no_laps4()," laps in 24 hours","<br>";
-echo "Team Honda drove ".$race->no_laps5()," laps in 24 hours","<br>";
-echo "Team Yamaha drove ".$race->no_laps6()," laps in 24 hours","<br>";
-echo "Team Kawasaki drove ".$race->no_laps7()," laps in 24 hours","<br>";
-echo "Team Suzuki drove ".$race->no_laps8()," laps in 24 hours","<br>";
+echo "<table  border='1' width='400'>";
+echo "<tr><th>No.</th><th>Team :</th><th>No. of laps in 24 hours:</th></tr>";
+echo "<tr><td> 1 </td><td> $crew_name1 </td><td> ".$race->no_laps1()," </td>";
+echo "<tr><td> 2 </td><td> $crew_name2 </td><td> ".$race->no_laps2()," </td>";
+echo "<tr><td> 3 </td><td> $crew_name3 </td><td> ".$race->no_laps3()," </td>";
+echo "<tr><td> 4 </td><td> $crew_name4 </td><td> ".$race->no_laps4()," </td>";
+echo "<tr><td> 5 </td><td> $crew_name5 </td><td> ".$race->no_laps5()," </td>";
+echo "<tr><td> 6 </td><td> $crew_name6 </td><td> ".$race->no_laps6()," </td>";
+echo "<tr><td> 7 </td><td> $crew_name7 </td><td> ".$race->no_laps7()," </td>";
+echo "<tr><td> 8 </td><td> $crew_name8 </td><td> ".$race->no_laps8()," </td></tr>";
+
+
+
 
